c27b10e9672573714d779d881429a8e5f60199c2
Magnus Simonsen <magnus.storjord.simonsen@gmail.com>
2025-11-03 15:24:41 +0100

    Update font size options in the MainWindow to include additional sizes

--- group-maker.py
+++ group-maker.py
@@ -255,7 +255,7 @@ class MainWindow(QMainWindow):
         
         # Font size selector
         self.font_size_combo = QComboBox()
-        self.font_size_combo.addItems(["8", "9", "10", "11", "12", "14", "16", "18", "20", "24"])
+        self.font_size_combo.addItems(["8", "9", "10", "11", "12", "13", "14", "15", "16", "18", "20", "24"])
         self.font_size_combo.setCurrentText("10")  # Default font size
         self.font_size_combo.currentTextChanged.connect(self.on_font_size_changed)
         
